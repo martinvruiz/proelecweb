abb35598d0a8cb7f7121d5f963dbed6a2f774577
Martin Villaló Ruiz <martinvruiz10@gmail.com>
2025-08-07 13:09:03 -0300

    changed when the animation starts

--- src/app/contacto/page.jsx
+++ src/app/contacto/page.jsx
@@ -10,7 +10,6 @@ export default function page() {
       initial={{ opacity: 0, y: 50 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.6 }}
-      viewport={{ once: true, amount: 0.2 }}
       className="flex flex-col items-center w-full my-4"
     >
       <h2 className="text-4xl text-[#1B2A41] p-4">Contactate con nosotros!</h2>
--- src/app/proveedores/page.jsx
+++ src/app/proveedores/page.jsx
@@ -12,7 +12,6 @@ export default function page() {
         initial={{ opacity: 0, y: 50 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6 }}
-        viewport={{ once: true, amount: 0.2 }}
         className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 mt-8"
       >
         {proveedores.map((prov, index) => {
