39b57b5d662081abd6798575dcdd1e941ac43f67
Martin Villaló Ruiz <martinvruiz10@gmail.com>
2025-08-07 13:07:26 -0300

    changed language

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body className={`${oswald.className} antialiased`}>
         <Container>
           <Navbar />
